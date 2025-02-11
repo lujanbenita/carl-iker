9635df05a938b09f5cf6262f878c0e5757747bfc
Carlos Luján <lujanbenita@gmail.com>
2025-02-11 21:54:03 +0100

    add quantity input

--- pages/calculadora.js
+++ pages/calculadora.js
@@ -14,6 +14,7 @@ const copyToClipboard = value => {
 const Calculadora = () => {
 	const [amount, setAmount] = useState(null);
 	const [result, setResult] = useState(null);
+	const [quantity, setQuantity] = useState(1);
 
 	useEffect(() => {
 		const amountValue = parseFloat(amount);
@@ -53,10 +54,27 @@ const Calculadora = () => {
 		});
 	};
 
+	const increase = () => {
+		setQuantity(prev => (prev !== null ? parseFloat(prev) + 1 : 1));
+	};
+
+	const decrease = () => {
+		setQuantity(prev => {
+			const newValue = prev !== null ? parseFloat(prev) - 1 : 0;
+			return newValue > 0 ? newValue : null;
+		});
+	};
+
 	const handleChange = e => {
 		const value = e.target.value;
 		setAmount(value === "" ? null : parseFloat(value));
 		copyToClipboard(value === "" ? 0 : parseFloat(value));
+	};
+
+	const handleQuantity = e => {
+		const value = e.target.value;
+		console.log("value", value);
+		setQuantity(value === "" ? null : parseFloat(value));
 	};
 
 	return (
@@ -104,6 +122,50 @@ const Calculadora = () => {
 							</div>
 							<p className="total">
 								Total: <span className="total-price"> {result.totalConIVA.toFixed(2)} €</span>
+							</p>
+						</div>
+					)}
+				</div>
+
+				<div className="results">
+					<p className="quantity-text">Unidades</p>
+					{result && (
+						<div className="result-with-iva result">
+							<div className="result-container">
+								<h3>Precio SIN IVA:</h3>
+								<p>IVA: {(result.IVA * quantity).toFixed(2)} €</p>
+								<p>RE: {(result.recargoEquivalenciaSinIVA * quantity).toFixed(2)} €</p>
+							</div>
+							<p className="total">
+								Total: <span className="total-price"> {(quantity * result.totalSinIVA).toFixed(2)} €</span>
+							</p>
+						</div>
+					)}
+					<div className="input-container">
+						<button className="quantity-btn" onClick={decrease}>
+							-
+						</button>
+						<input
+							type="number"
+							className="quantity-input"
+							min="0"
+							step="1"
+							value={quantity || ""}
+							onChange={handleQuantity}
+						/>
+						<button className="quantity-btn" onClick={increase}>
+							+
+						</button>
+					</div>
+
+					{result && (
+						<div className="result-without-iva result">
+							<div className="result-container">
+								<h3>Precio CON IVA:</h3>
+								<p>RE: {(result.recargoEquivalenciaConIVA * quantity).toFixed(2)} €</p>
+							</div>
+							<p className="total">
+								Total: <span className="total-price"> {(result.totalConIVA * quantity).toFixed(2)} €</span>
 							</p>
 						</div>
 					)}
